db45dea14f0c02a6186a15f794f016bdef8e8d9a
richardnidhog <112019620+richardnidhog@users.noreply.github.com>
2023-11-03 07:50:14 -0500

    Add files via upload

--- automatic_tiktok_analysis.py
+++ automatic_tiktok_analysis.py
@@ -1,12 +1,17 @@
 import os
 import openai
 import streamlit as st
+import requests
 import pandas as pd
+import datetime
 
-st.title("Automatic TikTok Misinformation Analysis")
+st.title("Automatic Misinformation Analysis")
+
+
+
 video_files = st.file_uploader('Upload Your Video File', type=['wav', 'mp3', 'mp4'], accept_multiple_files=True)
 
-openai.api_key = "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" #insert API key
+openai.api_key = "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" 
 
 def transcribe(video_file):
     result = openai.Audio.transcribe("whisper-1", video_file)
@@ -15,20 +20,27 @@ def transcribe(video_file):
     return transcript
 
 def analyze(transcript):
-    # Define the system message
-    system_msg = 'You are a helpful assistant who can understand when a statement contains misinformation.'
+    system_msg1 = "Your task is to determine if the following statement contains any misinformation. Begin by stating \'May contain misinformation\', \'Cannot be recognized\' or \'No misinformation detected\'."
 
-    # Define the user message
-    user_msg = 'Determine if the statement above contains any misinformation. In the part one, the results are first displayed in the first line, \'May contain misinformation\', \'Cannot be recognized at this time\' or \'No misinformation detected\'. In the part two, an operation is performed to extract keywords from the input text, providing up to six keywords, sorted in order of criticality. In the part three, briefly summarize the reasons for determining whether it contains misinformation. Three reasons of no more than 50 words each are required. Add line breaks between each section. Regardless of the state of the text given, it must be answered in the format given above:' + transcript
+    system_msg2 = "Your next task is to extract up to six keywords from the statement, sorted in order of criticality. Follow the format \"Keywords: ...\""
 
-    # Create a dataset using GPT
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
-                                            messages=[{"role": "system", "content": system_msg},
-                                            {"role": "user", "content": user_msg}])
-    
+    system_msg3 = "Lastly, you must briefly summarize the reasons for determining whether the statement contains misinformation. Provide three or less reasons of no more than 50 words each."
+
+    chat_sequence = [
+        {"role": "system", "content": "You are a helpful assistant. You need to complete the requirements based on the following paragraph." + transcript},
+        {"role": "user", "content": system_msg1},
+        {"role": "user", "content": system_msg2},
+        {"role": "user", "content": system_msg3}
+    ]   
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=chat_sequence
+    )
+
     content = response['choices'][0]['message']['content']
     gpt_response = '{}'.format(content)
-    return gpt_response # find a way to extract GPT text and store in variable, follow same format (transcript = '"{}"'.format(text))
+    return gpt_response
 
 @st.cache_data
 def convert_df(df):
@@ -41,22 +53,35 @@ if st.button('Transcribe and Analyze Videos'):
     for video_file in video_files:
         print(video_file.name)
         if video_file is not None:
-            st.sidebar.success('Transcribing' + " " + video_file.name)
+            st.sidebar.success('Analyzing' + " " + video_file.name)
             transcript = transcribe(video_file)
             analysis = analyze(transcript)
             st.markdown(video_file.name + ": " + transcript)
             st.markdown(video_file.name + ": " + analysis)
-            
-            d = {"transcript": transcript, "analysis": analysis}
+
+            # Split the analysis information
+            analysis = analysis.split('\n')
+            misinformation_status = analysis[0]
+            keywords = analysis[2]
+            #reasons = ' '.join(analysis[4:])
+
+            d = {
+                "video_file": video_file.name,
+                "transcript": transcript, 
+                "misinformation_status": misinformation_status, 
+                "keywords": keywords, 
+                #"reasons": reasons
+            }
             data.append(d)
-            #print('"{}"'.format(transcript))
+            #print('"{}"'.format(transcript))        
         else:
             st.sidebar.error("Please upload a video file")
     df = pd.DataFrame(data)
     csv = convert_df(df)
+    date_today = datetime.date.today().strftime('%Y_%m_%d')
     st.download_button(
-        label="Download transcipts as CSV",
+        label="Download results as CSV",
         data=csv,
-        file_name='transcripts.csv',
-        mime='transcripts/csv',
+        file_name = f'results_{date_today}.csv',
+        mime='text/plain',
     )
